3c48617d1bf23ff66e09d3ca39f8e0a1260d0d25
luisriv1321 <luki00r0@gmail.com>
2023-06-29 08:35:55 -0600

    proyecto inicializado

---
+++ src/main/java/com/mycompany/fina/Fina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fina;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Fina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
