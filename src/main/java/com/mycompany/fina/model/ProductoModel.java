608066e7d3ed15bf890c6b6b37c4583788313451
luisriv1321 <luki00r0@gmail.com>
2023-06-29 09:01:23 -0600

    clase

--- src/main/java/com/mycompany/fina/model/ProductoModel.java
+++ src/main/java/com/mycompany/fina/model/ProductoModel.java
@@ -8,6 +8,61 @@ package com.mycompany.fina.model;
  *
  * @author LENOVO
  */
-public class ProductoModel {
+public class ProductoModel { 
+    private String nombre;
+    private Integer cantidad;
+    private Double precio;
+    private String descripcion;
+
+    public ProductoModel() {
+    }
+
+    public ProductoModel(String nombre, Integer cantidad, Double precio, String descripcion) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductoModel{" + "nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio + ", descripcion=" + descripcion + '}';
+    }
+
+   
+
+   
     
 }
